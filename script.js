d2b0242062894f694c6ed70cd6dcad606f909548
Maciej <m.rybkowski@icloud.com>
2022-08-28 00:03:55 +0200

    input changes and turns added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ howManyPlayers.innerText = 'How many players?';
 const howManyPlayersInput = document.createElement('input');
 howManyPlayersInput.setAttribute('id', 'playerNumber');
 howManyPlayers.setAttribute('class', 'playerBox');
+howManyPlayersInput.setAttribute('type', 'number');
 howManyPlayersInput.setAttribute('value', 'min 2 max 6');
 howManyPlayersInput.setAttribute(
     'onclick',
@@ -68,14 +69,19 @@ function nameAccepted(i) {
 
     const numberSelect = document.createElement('input');
     numberSelect.setAttribute('id', `numberOfPlayer${i}`);
-    numberSelect.setAttribute('value', 'enter your number');
+    numberSelect.setAttribute('type', 'number');
 
     const numberAccept = document.createElement('button');
     numberAccept.setAttribute('id', `numberButton${i}`);
     numberAccept.innerText = 'Accept your number';
     numberAccept.setAttribute('onclick', `numberAccept(${i})`);
 
+    const numberText = document.createElement('div');
+    numberText.setAttribute('id', `text${i}`);
+    numberText.innerText = `enter your number`;
+
     playerBox.appendChild(enteredName);
+    playerBox.appendChild(numberText);
     playerBox.appendChild(numberSelect);
     playerBox.appendChild(numberAccept);
 }
@@ -85,19 +91,28 @@ function numberAccept(i) {
     const numberButton = document.getElementById(`numberButton${i}`);
     const numberOfPlayer = document.getElementById(`numberOfPlayer${i}`);
     const number = document.getElementById(`numberOfPlayer${i}`).value;
+    const numberText = document.getElementById(`text${i}`);
 
     numbersChosenByPlayers.splice(i, 0, number);
 
     playerBox.removeChild(numberButton);
     playerBox.removeChild(numberOfPlayer);
+    playerBox.removeChild(numberText);
 
     const selectedNumber = document.createElement('div');
     selectedNumber.innerText = `${number}`;
 
     const playButton = document.createElement('button');
-    playButton.setAttribute('id', `playButtonNr${i}`);
-    playButton.setAttribute('onclick', `playGame(${number},${i})`);
-    playButton.innerText = 'Throw dice';
+    if (i == 0) {
+        playButton.setAttribute('id', `playButtonNr${i}`);
+        playButton.setAttribute('onclick', `playGame(${number},${i})`);
+        playButton.innerText = 'Throw dice';
+    } else {
+        playButton.setAttribute('id', `playButtonNr${i}`);
+        playButton.setAttribute('onclick', `playGame(${number},${i})`);
+        playButton.innerText = 'Not your turn';
+        playButton.disabled = true;
+    }
 
     playerBox.appendChild(selectedNumber);
     playerBox.appendChild(playButton);
@@ -105,6 +120,21 @@ function numberAccept(i) {
 
 function playGame(num, x) {
     const playerBox = document.getElementById(`playerBox${x}`);
+    if (x < playersNumber - 1) {
+        const nextPlayerButton = document.getElementById(
+            `playButtonNr${x + 1}`
+        );
+        nextPlayerButton.innerText = 'Throw dice';
+        nextPlayerButton.disabled = false;
+    } else {
+        const nextPlayerButton = document.getElementById(`playButtonNr${0}`);
+        nextPlayerButton.innerText = 'Throw dice';
+        nextPlayerButton.disabled = false;
+    }
+
+    const nextPlayerButton = document.getElementById(`playButtonNr${x}`);
+    nextPlayerButton.innerText = 'Not your turn';
+    nextPlayerButton.disabled = true;
 
     if (isThisFirstGame[x] == 1) {
         const oldPlayerBox = document.getElementById(`numberGotten${x}`);
@@ -117,11 +147,19 @@ function playGame(num, x) {
     numberGotten.setAttribute('id', `numberGotten${x}`);
     numberGotten.innerText = `Number ${randomNumber} was rolled, it was nobody's number`;
 
+    let won = 0;
+
     for (let i = 0; i < playersNames.length; i++) {
-        if (randomNumber == numbersChosenByPlayers[i]) {
+        if (randomNumber == numbersChosenByPlayers[i] && randomNumber != num) {
+            won++;
             numberGotten.innerText = `You rolled ${randomNumber} and it was ${playersNames[i]} number!`;
+            numberGotten.style.cssText = 'color:red; font-size:20px';
         }
     }
+    if (won == 0) {
+        numberGotten.style.cssText = '';
+    }
+    console.log(` number ${randomNumber} for player ${x + 1}`);
     playerBox.appendChild(numberGotten);
 }
 const isThisFirstGame = [];
